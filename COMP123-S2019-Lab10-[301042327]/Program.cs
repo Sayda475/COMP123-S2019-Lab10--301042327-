584f7a95f85fdab717919aa60da0131198ca8f7e
sayda.pharma <sayda.pharma@gmail.com>
2019-07-19 13:51:21 -0400

    Add event handler for back button and next button and also use splash timer

--- COMP123-S2019-Lab10-[301042327]/Program.cs
+++ COMP123-S2019-Lab10-[301042327]/Program.cs
@@ -16,7 +16,7 @@ namespace COMP123_S2019_Lab10__301042327_
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new StartForm());
         }
     }
 }
